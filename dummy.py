4c318e76e34ffd6bdfa528262bc31383edd235ed
khoa <duongvankhoadt@gmail.com>
2025-10-19 22:06:16 +0800

    Nothing

---
+++ dummy.py
@@ -0,0 +1,14 @@
+# import os
+# import shutil
+
+# src_dir = 'data/coco/train2017'
+# dest_dir = 'data/OWOD/JPEGImages'
+
+# filenames = os.listdir(src_dir)
+# for filename in filenames:
+#     shutil.copy(os.path.join(src_dir, filename), os.path.join(dest_dir, filename))
+    
+    
+    
+import os
+print(os.listdir('/home/khoa/projects/OOD_OD/OWOBJ_Exploring/data/OWOD/Annotations'))
